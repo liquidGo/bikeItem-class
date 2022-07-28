0d77a4434f6df273c20dcc53d2affaa93b42bd69
aaaddf <eee@qq.com>
2022-07-28 12:51:08 +0800

    十三章柱状图完成

--- src/pages/echarts/bar/Bar.jsx
+++ src/pages/echarts/bar/Bar.jsx
@@ -29,7 +29,7 @@ export default class Bar extends Component {
                 trigger: 'axis'
             }
             ,
-         
+
             xAxis: {
                 data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
             },
